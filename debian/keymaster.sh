cfe78cfdfcffd5b63389b0763f79ae09cf0f7a9b
Rene Malmgren <duke_h3@dwdc.club>
2026-07-26 11:05:36 +0200

    Add NOSTR_SA_SOCK to environment.d and profile.d
    Propagate the Nostr SA socket path automatically in systemd
    sessions (environment.d) and shell logins (profile.d with socket
    existence guard).

--- debian/keymaster.sh
+++ debian/keymaster.sh
@@ -4,3 +4,6 @@ fi
 if [ -d "$XDG_RUNTIME_DIR/gnupg-keymaster" ]; then
     export GNUPGHOME="$XDG_RUNTIME_DIR/gnupg-keymaster"
 fi
+if [ -S "$XDG_RUNTIME_DIR/keymaster-nostr-sa.sock" ]; then
+    export NOSTR_SA_SOCK="$XDG_RUNTIME_DIR/keymaster-nostr-sa.sock"
+fi
